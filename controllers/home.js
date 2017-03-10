5d1590652fc07221561f8990ec8a583342b29d53
TheGum <xyybab00ba99@gmail.com>
2017-03-10 09:43:09 +0200

    Login is successful.

--- controllers/home.js
+++ controllers/home.js
@@ -6,26 +6,33 @@ module.exports = {
         driver.get('http://www.neobux.com');
 
         // Check if user is logged
-        try {
-            driver.findElement({ xpath: '/html/body/div[1]/div[1]/table/tbody/tr[2]/td[2]/table/tbody/tr/td[1]/a[4]'}).click();
+        driver
+            .findElement({ xpath: '/html/body/div[1]/div[1]/table/tbody/tr[2]/td[2]/table/tbody/tr/td[1]/a[4]'})
+            .then((element) => { // User is not logged
+                    // Check for image authentication
+                    element.click();
 
-             // Check for image authentication
-            try {
-                // Get image src
-                let link = driver
-                    .findElement({ xpath: '//*[@id="Kf0"]/tbody/tr[1]/td[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td[2]/img'})
-                    .getAttribute('src');
+                    driver
+                        .findElement({ xpath: '//*[@id="Kf0"]/tbody/tr[1]/td[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td[2]/img'})
+                        .then((element) => {
+                            // get image
+                            let link = element.getAttribute('src');
+                            // render image in extension
+                            res.render('user/login', { image: true, imageLink: link});
 
-                // render image in extension
-                res.render('user/login', { image: true, imageLink: link});
-            }
+                        }, (err) => {
+                            if (err) {
+                                console.log('No image authentication!');
+                                res.render('user/login', { image: false})
+                            }
+                        });
 
-            res.render('user/login', { image: false})
-
-        } catch (NoSuchElementException) {
-            res.render('home/profile')
-        }
-
+        }, (err) => {
+                if (err) {
+                    console.log('User is logged!');
+                    res.render('home/profile');
+                }
+            });
 
     },
     loginPost: (req, res) => {
@@ -35,15 +42,25 @@ module.exports = {
         driver.findElement({ xpath: '//*[@id="Kf1"]' }).sendKeys(registerArgs.username);
         // send password from the extension to neobux password field
         driver.findElement({ xpath: '//*[@id="Kf2"]' }).sendKeys(registerArgs.password);
+
+        // send image code from the extension to neobux code field
+        if (registerArgs.code) driver.findElement({ xpath: '//*[@id="Kf3"]'}).sendKeys(registerArgs.code);
+
         // click neobux login button
         driver.findElement({ xpath: '//*[@id="botao_login"]' }).click();
 
-        // TODO: Check if login was successful
+        driver.findElement({ xpath: '//*[@id="ubar_w1"]/tbody/tr/td[3]/a'})
+            .then(() => {
+                console.log('Login successful')
+            }, (err) => {
+                if (err) console.log(err)
+            });
 
         // open neobux advertisements page
         driver.findElement({ xpath: '/html/body/div[1]/div[1]/table/tbody/tr[3]/td/table/tbody/tr/td[3]/a'}).click();
 
         // render profile view in extension
         res.render('home/profile')
+
     }
 };
